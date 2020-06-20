ee3e325203f1483209207f0afe7c2af5a9211370
JcdeA <31413538+JcdeA@users.noreply.github.com>
2020-06-20 21:08:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
 import psutil, webbrowser,time,pyautogui, os
 from multiprocessing import Process
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from collections import deque
-import numpy as np
+
 
 os.system('killall firefox')
 
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 200), ylim=(0, psutil.virtual_memory().total))
-line, = ax.plot([],[])
-plt.title("Ram usage")
-y_list = deque([-1]*400)
-x_list = deque(np.linspace(200,0,num=400))
 
-def init():
-    line.set_data([],[])
-    return line,
-
-
-def animate(i):
-    y_list.pop()
-    y_list.appendleft(psutil.virtual_memory().used)
-    line.set_data(x_list,y_list)
-    return line,
-
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                           frames=200, interval=100, blit=True)
-def show():
-    plt.show()
 
 
 
@@ -54,7 +30,9 @@ def main():
 
             exit()
         i+=1
-p = Process(target = show)
+
 m = Process(target = main)
+
+
 #p.start()
 m.start()
