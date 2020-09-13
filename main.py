6294c1c6b0fc71f039bfed8d9d30d63991fd812e
jcdeA <31413538+JcdeA@users.noreply.github.com>
2020-09-14 08:54:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-'''copyright 2020 by JcdeA
-    ok please don't judge me by how this is written
-    I wasnt really trying my best.
-'''
+
 import psutil, webbrowser,time,pyautogui, os
 from multiprocessing import Process
 
 
-os.system('killall firefox')#UNLIMITED POWER!!!!!!!
+os.system('killall firefox')
 
 
 
@@ -21,13 +18,13 @@ def main():
 
 
 
-        webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')#oh yah
+        webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
         os.system('wmctrl -a firefox')
         time.sleep(3+i*0.2)
         pyautogui.typewrite(' ')
         print(i)
 
-        mem = psutil.virtual_memory()F
+        mem = psutil.virtual_memory()
         if mem.available <= THRESHOLD:
             print('tested ' + str(i) + ' tabs. exiting now......')
             os.system('killall firefox')
